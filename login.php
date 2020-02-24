88d8f9ba2113303d67d81f9b7a31ead37941b824
Daniel <daniel.kapexhiu@gmail.com>
2020-02-24 21:31:11 +0100

    Update login.php

--- login.php
+++ login.php
@@ -57,7 +57,7 @@
               //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
               //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
               //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-              firebase.auth.EmailAuthProvider.PROVIDER_ID
+              //firebase.auth.EmailAuthProvider.PROVIDER_ID
             ],
             // Terms of service url.
             'tosUrl': false,
